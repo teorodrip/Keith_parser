8f9cb40e4985bd9eb23e872da5113e24c46ae47f
unchartech_2 <mateo.rodriguez@unchartech.com>
2019-01-31 17:50:10 +0100

    changed parser so it can read from directory

--- parser/srcs/main.cpp
+++ parser/srcs/main.cpp
@@ -6,13 +6,52 @@
 //   By: Mateo <teorodrip@protonmail.com>                                     //
 //                                                                            //
 //   Created: 2019/01/04 17:51:41 by Mateo                                    //
-//   Updated: 2019/01/31 14:45:14 by Mateo                                    //
+//   Updated: 2019/01/31 17:32:25 by Mateo                                    //
 //                                                                            //
 // ************************************************************************** //
 
 #include "../includes/parser.hpp"
+#include <dirent.h>
 
-int main()
+static void parse_from_dir(std::string path)
+{
+	DIR *dir;
+	struct dirent *ent;
+	excel_parser parser;
+	std::string file_string;
+	std::string path_to_file;
+	// std::regex file_format (".*\\.xlsx");
+	std::regex file_format ("^(([^~])+).*\\.xlsx");
+
+	if ((dir = opendir(path.c_str())) == NULL)
+		{
+			printf("Error: opening dir %s\n", path.c_str());
+			return;
+		}
+	parser.init();
+	while ((ent = readdir(dir)) != NULL)
+		{
+			file_string = ent->d_name;
+			if (!std::regex_match(file_string.begin(), file_string.end(), file_format))
+				continue;
+			parser.client::signal_parsing();
+			printf("Parsing %s\n", ent->d_name);
+			path_to_file = path + file_string;
+			parser.load_book(path_to_file);
+			parser.parse_book();
+			parser.client::signal_end_parsing();
+			printf("Done\n");
+			parser.clear_all();
+			// goto label;
+		}
+	// label:
+	closedir(dir);
+	parser.clear_bloom_tickers();
+	parser.clear_ticker_retries();
+	parser.data_base::finish_db();
+}
+
+static void parse_from_watcher()
 {
 	excel_parser parser;
 	unsigned char n_vm;
@@ -52,35 +91,20 @@ int main()
 			usleep(9000000);
 		}
 	while (parser.client::check_end());
- // label:
+	// label:
 	parser.clear_bloom_tickers();
 	parser.clear_ticker_retries();
 	parser.data_base::finish_db();
 	delete[] watcher;
+}
 
-	// xlsxioreader book;
-	// xlsxioreadersheet sheet;
-	// char *cell_value;
-	// char *sheet_name = NULL;
-
-	// if ((book = xlsxioread_open(FILE_NAME)) == NULL)
-	// 	{
-	// 		std::cerr << "Error opening: " << FILE_NAME << "\n";
-	// 		exit(2);
-	// 	}
-	// if ((sheet = xlsxioread_sheet_open(book, sheet_name, XLSXIOREAD_SKIP_EMPTY_ROWS)) != NULL)
-	// 	{
-	// 		while (xlsxioread_sheet_next_row(sheet))
-	// 			{
-	// 				while ((cell_value = xlsxioread_sheet_next_cell(sheet)) != NULL)
-	// 					{
-	// 						std::cout << cell_value << "\t";
-	// 						free(cell_value);
-	// 					}
-	// 				std::cout << "\n";
-	// 			}
-	// 		xlsxioread_sheet_close(sheet);
-	// 	}
-	// xlsxioread_close(book);
-	// return (0);
+int main(int argc, char **argv)
+{
+	if (argc == 1)
+		parse_from_watcher();
+	else if (argc == 2)
+		parse_from_dir(argv[1]);
+	else
+		printf("Nothing to do\n");
+	return (1);
 }
